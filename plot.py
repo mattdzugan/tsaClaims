e1e02b7f7a5073f71e3cbe75c1f8bf32f2cee54b
Matt Dzugan <mattdzugan@gmail.com>
2018-08-31 08:25:41 -0500

    i think finished

--- plot.py
+++ plot.py
@@ -47,6 +47,7 @@ for idx in range(8,33):
 
     thisWinner = {'idx':  idx-8,
                   'val':  100.0*(thisWinnerData['target']-gobalAvgValue)/gobalAvgValue,
+                  'valRatio':  thisWinnerData['target']/gobalAvgValue,
                   'globalAvgVal':  gobalAvgValue,
                   'winnerVal':     thisWinnerData['target'],
                   'Catg': categoryOfInterest,
@@ -57,151 +58,151 @@ for idx in range(8,33):
     winnerStats.append(thisWinner)
 
 
-    # Create grid values first.
-    xi = np.linspace(-180, -65, 115*pointsPerDegree+1)
-    yi = np.linspace(10, 75, 65*pointsPerDegree+1)
-    Xi, Yi = np.meshgrid(xi, yi)
-
-
-    # we'll bbe doing a weighted average so lets define the weights
-    global myTrainingWeights
-    myTrainingWeights = np.array(airports_df['meanEnplaned'])
-
-    # and our "training" data for our interpolation
-    trainingX = np.column_stack((airports_df['Lon'], airports_df['Lat']))
-    trainingY = np.array(airports_df['target'])
-
-
-    # actually perform the interpolation on our grid of points
-    N_neighbors_min = 40
-    R_max = 250
-    def greatCircleDistance(lon1,lat1,lon2,lat2):
-        deg2rad = math.pi/180.0
-        lon1r = lon1*deg2rad
-        lat1r = lat1*deg2rad
-        lon2r = lon2*deg2rad
-        lat2r = lat2*deg2rad
-        if (abs(lon1-lon2)+abs(lat1-lat2)>0.0000001):
-            centralAngle = math.acos( math.sin(lat1r)*math.sin(lat2r) + math.cos(lat1r)*math.cos(lat2r)*math.cos(lon2r-lon1r)  )
-        else:
-            centralAngle = 0
-        return centralAngle*6378
-
-    predCoords = np.column_stack((Xi.reshape(-1,1),Yi.reshape(-1,1)))
-    N_points = predCoords.shape[0]
-    N_training = trainingX.shape[0]
-    predVals = np.zeros(N_points)
-
-    for ii in range(N_points):
-        thisPoint = predCoords[ii]
-
-        distToTrainings = np.zeros(N_training)
-        for jj in range(N_training):
-            thisTraining = trainingX[jj]
-            distToTrainings[jj] = greatCircleDistance(thisPoint[0],thisPoint[1],thisTraining[0],thisTraining[1])
-
-        # weighted avg of all within R_max (unless < N_neighbors_min... in which case jsut weight nearest N )
-        weightQuotients = myTrainingWeights / (distToTrainings**2)
-        # which ones to use?
-        # weighted avg of all within R_max (unless < N_neighbors_min... in which case jsut weight nearest N )
-        withinRad = distToTrainings<R_max
-        NumWithinRad = sum(withinRad)
-        if (NumWithinRad >= N_neighbors_min):
-            # weighted avg of all within R_max
-            isUsed = withinRad
-        else:
-            # use the nearest N
-            sortedDists = np.sort(distToTrainings)
-            nthDist = sortedDists[N_neighbors_min]
-            isUsed = 1.00*(distToTrainings<(0.999999*nthDist))
-
-        myWeights = weightQuotients*isUsed
-        #if (sum(myWeights)<1):
-            #print(sortedDists)10
-        predVals[ii] = np.average(trainingY, weights=myWeights)
-
-    predVals.reshape(Xi.shape)
-    zi = predVals.reshape(Xi.shape)
-
-
-
-
-    # prepare zi for plotting
-    zi_diff = ((zi-gobalAvgValue)/gobalAvgValue)
-
-
-
-    # clip it at +/- 100% to maintain consistent viz across categories
-    maxDiff = 100.0
-    zi_diff_clipped = np.minimum(np.maximum(zi_diff,-maxDiff/100.0),maxDiff/100.0)
-    myLevels = np.linspace(-maxDiff/100.0, maxDiff/100.0, 101)
-
-
-
-    # get those sweet sweet contours
-    figure = plt.figure()
-    ax = figure.add_subplot(111)
-    mycontour = plt.contourf(xi, yi, zi_diff_clipped, myLevels, cmap=plt.cm.jet)
-    def collec_to_gdf(collec_poly):
-        """Transform a `matplotlib.contour.QuadContourSet` to a GeoDataFrame"""
-        polygons, colors, levels = [], [], []
-        for i, polygon in enumerate(collec_poly.collections):
-            mpoly = []
-            for path in polygon.get_paths():
-                try:
-                    path.should_simplify = False
-                    poly = path.to_polygons()
-                    # Each polygon should contain an exterior ring + maybe hole(s):
-                    exterior, holes = [], []
-                    if len(poly) > 0 and len(poly[0]) > 3:
-                        # The first of the list is the exterior ring :
-                        exterior = poly[0]
-                        # Other(s) are hole(s):
-                        if len(poly) > 1:
-                            holes = [h for h in poly[1:] if len(h) > 3]
-                    mpoly.append(Polygon(exterior, holes))
-                except:
-                    print('Warning: Geometry error when making polygon #{}'
-                          .format(i))
-            if len(mpoly) > 1:
-                mpoly = MultiPolygon(mpoly)
-                polygons.append(mpoly)
-                colors.append(polygon.get_facecolor().tolist()[0])
-                levels.append(myLevels[i]*100.0)
-            elif len(mpoly) == 1:
-                polygons.append(mpoly[0])
-                colors.append(polygon.get_facecolor().tolist()[0])
-                levels.append(myLevels[i]*100.0)
-        return GeoDataFrame(
-            geometry=polygons,
-            data={'RGBA': colors, 'levels':levels},
-            crs={'init': 'epsg:4326'})
-
-    contour_df = collec_to_gdf(mycontour)
-    plt.close(figure)
-
-
-    # but all my contours are on the giant rectangle - need to crop them to fit snugly in USA
-    # load USA
-    us_df = gpd.read_file('us-states.json')
-    usaPoly = us_df.buffer(0).unary_union
-
-
-    # crop 'em
-    N_shapes = len(contour_df.geometry)
-    for ii in range(N_shapes):
-        oldGeom = contour_df.geometry[ii]
-        newGeom = oldGeom.intersection(usaPoly)
-        contour_df.geometry[ii] = newGeom
-
-
-    # write the contour data
-    geojsonfilepath = "./viz/data/categoryContours/category_"+str(idx-8)+".geojson"
-    if (isByClaim):
-        geojsonfilepath = "./viz/data/categoryContours/category_"+str(idx-8)+"_byClaim.geojson"
-    f = open(geojsonfilepath, "w")
-    f.write(contour_df.to_json())
-    f.close()
+    # # Create grid values first.
+    # xi = np.linspace(-180, -65, 115*pointsPerDegree+1)
+    # yi = np.linspace(10, 75, 65*pointsPerDegree+1)
+    # Xi, Yi = np.meshgrid(xi, yi)
+    #
+    #
+    # # we'll bbe doing a weighted average so lets define the weights
+    # global myTrainingWeights
+    # myTrainingWeights = np.array(airports_df['meanEnplaned'])
+    #
+    # # and our "training" data for our interpolation
+    # trainingX = np.column_stack((airports_df['Lon'], airports_df['Lat']))
+    # trainingY = np.array(airports_df['target'])
+    #
+    #
+    # # actually perform the interpolation on our grid of points
+    # N_neighbors_min = 40
+    # R_max = 250
+    # def greatCircleDistance(lon1,lat1,lon2,lat2):
+    #     deg2rad = math.pi/180.0
+    #     lon1r = lon1*deg2rad
+    #     lat1r = lat1*deg2rad
+    #     lon2r = lon2*deg2rad
+    #     lat2r = lat2*deg2rad
+    #     if (abs(lon1-lon2)+abs(lat1-lat2)>0.0000001):
+    #         centralAngle = math.acos( math.sin(lat1r)*math.sin(lat2r) + math.cos(lat1r)*math.cos(lat2r)*math.cos(lon2r-lon1r)  )
+    #     else:
+    #         centralAngle = 0
+    #     return centralAngle*6378
+    #
+    # predCoords = np.column_stack((Xi.reshape(-1,1),Yi.reshape(-1,1)))
+    # N_points = predCoords.shape[0]
+    # N_training = trainingX.shape[0]
+    # predVals = np.zeros(N_points)
+    #
+    # for ii in range(N_points):
+    #     thisPoint = predCoords[ii]
+    #
+    #     distToTrainings = np.zeros(N_training)
+    #     for jj in range(N_training):
+    #         thisTraining = trainingX[jj]
+    #         distToTrainings[jj] = greatCircleDistance(thisPoint[0],thisPoint[1],thisTraining[0],thisTraining[1])
+    #
+    #     # weighted avg of all within R_max (unless < N_neighbors_min... in which case jsut weight nearest N )
+    #     weightQuotients = myTrainingWeights / (distToTrainings**2)
+    #     # which ones to use?
+    #     # weighted avg of all within R_max (unless < N_neighbors_min... in which case jsut weight nearest N )
+    #     withinRad = distToTrainings<R_max
+    #     NumWithinRad = sum(withinRad)
+    #     if (NumWithinRad >= N_neighbors_min):
+    #         # weighted avg of all within R_max
+    #         isUsed = withinRad
+    #     else:
+    #         # use the nearest N
+    #         sortedDists = np.sort(distToTrainings)
+    #         nthDist = sortedDists[N_neighbors_min]
+    #         isUsed = 1.00*(distToTrainings<(0.999999*nthDist))
+    #
+    #     myWeights = weightQuotients*isUsed
+    #     #if (sum(myWeights)<1):
+    #         #print(sortedDists)10
+    #     predVals[ii] = np.average(trainingY, weights=myWeights)
+    #
+    # predVals.reshape(Xi.shape)
+    # zi = predVals.reshape(Xi.shape)
+    #
+    #
+    #
+    #
+    # # prepare zi for plotting
+    # zi_diff = ((zi-gobalAvgValue)/gobalAvgValue)
+    #
+    #
+    #
+    # # clip it at +/- 100% to maintain consistent viz across categories
+    # maxDiff = 100.0
+    # zi_diff_clipped = np.minimum(np.maximum(zi_diff,-maxDiff/100.0),maxDiff/100.0)
+    # myLevels = np.linspace(-maxDiff/100.0, maxDiff/100.0, 101)
+    #
+    #
+    #
+    # # get those sweet sweet contours
+    # figure = plt.figure()
+    # ax = figure.add_subplot(111)
+    # mycontour = plt.contourf(xi, yi, zi_diff_clipped, myLevels, cmap=plt.cm.jet)
+    # def collec_to_gdf(collec_poly):
+    #     """Transform a `matplotlib.contour.QuadContourSet` to a GeoDataFrame"""
+    #     polygons, colors, levels = [], [], []
+    #     for i, polygon in enumerate(collec_poly.collections):
+    #         mpoly = []
+    #         for path in polygon.get_paths():
+    #             try:
+    #                 path.should_simplify = False
+    #                 poly = path.to_polygons()
+    #                 # Each polygon should contain an exterior ring + maybe hole(s):
+    #                 exterior, holes = [], []
+    #                 if len(poly) > 0 and len(poly[0]) > 3:
+    #                     # The first of the list is the exterior ring :
+    #                     exterior = poly[0]
+    #                     # Other(s) are hole(s):
+    #                     if len(poly) > 1:
+    #                         holes = [h for h in poly[1:] if len(h) > 3]
+    #                 mpoly.append(Polygon(exterior, holes))
+    #             except:
+    #                 print('Warning: Geometry error when making polygon #{}'
+    #                       .format(i))
+    #         if len(mpoly) > 1:
+    #             mpoly = MultiPolygon(mpoly)
+    #             polygons.append(mpoly)
+    #             colors.append(polygon.get_facecolor().tolist()[0])
+    #             levels.append(myLevels[i]*100.0)
+    #         elif len(mpoly) == 1:
+    #             polygons.append(mpoly[0])
+    #             colors.append(polygon.get_facecolor().tolist()[0])
+    #             levels.append(myLevels[i]*100.0)
+    #     return GeoDataFrame(
+    #         geometry=polygons,
+    #         data={'RGBA': colors, 'levels':levels},
+    #         crs={'init': 'epsg:4326'})
+    #
+    # contour_df = collec_to_gdf(mycontour)
+    # plt.close(figure)
+    #
+    #
+    # # but all my contours are on the giant rectangle - need to crop them to fit snugly in USA
+    # # load USA
+    # us_df = gpd.read_file('us-states.json')
+    # usaPoly = us_df.buffer(0).unary_union
+    #
+    #
+    # # crop 'em
+    # N_shapes = len(contour_df.geometry)
+    # for ii in range(N_shapes):
+    #     oldGeom = contour_df.geometry[ii]
+    #     newGeom = oldGeom.intersection(usaPoly)
+    #     contour_df.geometry[ii] = newGeom
+    #
+    #
+    # # write the contour data
+    # geojsonfilepath = "./viz/data/categoryContours/category_"+str(idx-8)+".geojson"
+    # if (isByClaim):
+    #     geojsonfilepath = "./viz/data/categoryContours/category_"+str(idx-8)+"_byClaim.geojson"
+    # f = open(geojsonfilepath, "w")
+    # f.write(contour_df.to_json())
+    # f.close()
 
 
 
--- viz/js/domBuilder.js
+++ viz/js/domBuilder.js
@@ -1,4 +1,4 @@
-d3.json("./data/winners.json", function(categories) {
+d3.json("./data/winners_byClaim.json", function(categories) {
 
   // get rid of the "other" one
   categories = categories.filter(function(item) {
@@ -15,10 +15,9 @@ d3.json("./data/winners.json", function(categories) {
 
   /*
   categories.sort(function(a, b) {
-      //return (a.globalAvgVal < b.globalAvgVal);
       return (a.globalAvgVal > b.globalAvgVal) ? -1 : (a.globalAvgVal < b.globalAvgVal) ? 1 : 0;
   });
-  */
+  //*/
 
 
   var theRow = d3.select(".row");
@@ -27,18 +26,30 @@ d3.json("./data/winners.json", function(categories) {
                           .enter()
                           .append("div")
                           .attr("class","categoryDiv col-12 col-sm-6 col-md-4");
+  /*
   myMultiple.append("img")
             .attr("class","img-fluid")
-            .attr("src",function(d){return ("./img/contour/contourMap_"+d.idx+".png");})
+            .attr("src",function(d){return ("./img/contour_byClaim/contourMap_"+d.idx+".png");});
+  */
+  var myFullImg = myMultiple.append("a")
+            .attr("class","img-fluid progressive replace")
+            .attr("href",function(d){return ("./img/contour_byClaim/contourMap_"+d.idx+".png");});
+  myFullImg.append("img")
+            .attr("class","img-fluid preview")
+            .attr("alt","image")
+            .attr("src","./img/preview.png");
+
+
+
   myMultiple.append("h3")
             .attr("class","categoryTitle")
             .html(function(d){return d.Catg});
   myMultiple.append("p")
             .attr("class","categoryAvg")
-            .html(function(d){return ("National Average: "+d3.format(",.2r")(d.globalAvgVal*1000.0*1000.0)+" claims per million passengers");});
+            .html(function(d){return ("National Average: "+d3.format(".1%")(d.globalAvgVal)+" claims are "+d.Catg);});
   myMultiple.append("p")
             .attr("class","categoryWinner")
-            .html(function(d){return (d.Name+" at "+d3.format(".2r")(d.val)+"% higher");});
+            .html(function(d){return (d.Name+" has "+d3.format(".2r")(d.valRatio)+" times as much");});
 
 
 });
